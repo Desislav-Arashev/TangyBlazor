13a6c678ff4faaebf02757d7b89144d6af8172e5
Desislav Arashev <desislav.arashev@gmail.com>
2022-07-29 16:33:35 +0000

    Validations in Blazor

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -9,8 +9,8 @@ namespace Tangy_Models
 {
     public class CategoryDTO
     {
-        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter name...")]
         public string Name { get; set; }
     }
 }
