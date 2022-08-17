780468536c5098a977c6ae462439fd1480271841
Desislav Arashev <desislav.arashev@gmail.com>
2022-08-17 23:25:55 +0000

    Product class, productDTO

---
+++ Tangy_Models/ProductDTO.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tangy_Models
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool ShopFavorite { get; set; }
+        public bool CustomerFavorite { get; set; }
+        public string Color { get; set; }
+        public string ImageUrl { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category")]
+        public int CategoryId { get; set; }
+        public CategoryDTO Category { get; set; }
+    }
+}
