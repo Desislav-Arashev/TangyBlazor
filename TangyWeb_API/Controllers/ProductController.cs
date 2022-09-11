a60cb73b02b64c12284db54f9d36ac6b0e23d05a
Desislav Arashev <desislav.arashev@gmail.com>
2022-09-11 13:09:51 +0000

    Product Controller API Endpoint

--- TangyWeb_API/Controllers/ProductController.cs
+++ TangyWeb_API/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Tangy_Business.Repository.IRepository;
+using Tangy_Models;
 
 namespace TangyWeb_API.Controllers
 {
@@ -15,6 +16,39 @@ namespace TangyWeb_API.Controllers
         public ProductController(IProductRepository productRepository)
         {
             _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productRepository.GetAll();
+            return Ok(products);
+        }
+
+
+        [HttpGet("{productId}")]
+        public async Task<IActionResult> Get(int? productId)
+        {
+            if (productId == null || productId == 0)
+            {
+                return BadRequest(new ErrorModelDTO()
+                {
+                    ErrorMessage = "Invalid Id",
+                    StatusCode = StatusCodes.Status400BadRequest
+                });
+            }
+            
+            var product = await _productRepository.Get(productId.Value);
+            if (product == null)
+            {
+                return BadRequest(new ErrorModelDTO()
+                {
+                    ErrorMessage = "Invalid Id",
+                    StatusCode = StatusCodes.Status404NotFound
+                });
+            }
+
+            return Ok(product);
         }
     }
 }
--- Tangy_Models/ErrorModelDTO.cs
+++ Tangy_Models/ErrorModelDTO.cs
@@ -7,7 +7,6 @@ namespace Tangy_Models
 {
     public class ErrorModelDTO
     {
-        public string Title { get; set; }
         public int StatusCode { get; set; }
         public string ErrorMessage { get; set; }
     }
